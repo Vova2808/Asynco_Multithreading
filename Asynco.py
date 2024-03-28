f73171910a9ce66bc23a386709703e863729c095
Vova <96084748+Vova2808@users.noreply.github.com>
2024-03-28 16:56:25 +0300

    Asynco.py

--- Asynco.py
+++ Asynco.py
@@ -1,22 +1,57 @@
-# Импортируем все нужные библиотеки для асинхронности
 import asyncio
 import time
-
-# Первая асинхронная функция
-async def say_hello():
-    print("Привет Мир!")
-
-# Вторая асинхронная функция
-async def say_goodbye(i):
-    print("Пока Мир!")
+#pip install colorama
+from colorama import init
+init()
+from colorama import Fore, Back, Style
 
 
-# Запуск одновремменно
-async def run_all_tasks():
-    await asyncio.gather(
-        say_hello(),
-        say_goodbye()
-    )
+print(f"{Fore.GREEN}-------------Асинхронные функции-------------{Style.RESET_ALL}")
 
-asyncio.run(run_all_tasks())
-print("Все функции были выполненны одновременно")
+async def fun1(x):
+    print(f"{x} возвести во вторую степень = {x**2}")
+    await asyncio.sleep(3)
+    print(f'{Fore.YELLOW}fun1 завершена{Style.RESET_ALL}')
+
+
+async def fun2(x):
+    print(f"{x} возвести в 0.5 степень = {x**0.5}")
+    await asyncio.sleep(3)
+    print(f'{Fore.YELLOW}fun2 завершена{Style.RESET_ALL}')
+
+
+print(f"{Fore.RED}Начало {time.strftime('%X')}{Style.RESET_ALL}")
+
+loop = asyncio.get_event_loop()
+task1 = loop.create_task(fun1(4))
+task2 = loop.create_task(fun2(4))
+loop.run_until_complete(asyncio.wait([task1, task2]))
+
+print(f"{Fore.RED}Конец {time.strftime('%X')}{Style.RESET_ALL}")
+
+print("")
+print(f"{Fore.GREEN}-------------Обычные функции-------------{Style.RESET_ALL}")
+time.sleep(2)
+
+
+##############################################################
+def fun1(x):
+    print(f"{x} возвести во вторую степень = {x**2}")
+    time.sleep(3)
+    print(f'{Fore.YELLOW}fun1 завершена{Style.RESET_ALL}')
+
+
+def fun2(x):
+    print(f"{x} возвести в 0.5 степень = {x**0.5}")
+    time.sleep(3)
+    print(f'{Fore.YELLOW}fun2 завершена{Style.RESET_ALL}')
+
+
+def main():
+    fun1(4)
+    fun2(4)
+
+print(f"{Fore.RED}Начало {time.strftime('%X')}{Style.RESET_ALL}")
+main()
+
+print(f"{Fore.RED}Конец {time.strftime('%X')}{Style.RESET_ALL}")
